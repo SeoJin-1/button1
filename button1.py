40bcb32e95365d1caad07a929f148b7592b9f496
YangSeoJin <didtjwls10@gmail.com>
2023-09-11 14:32:24 +0900

    print name and hakbun

--- button1.py
+++ button1.py
@@ -2,10 +2,9 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Hong kil-dong")
-b2 = Button(window, text="60201111")
+b1 = Button(window, text="Yang seo-jin")
+b2 = Button(window, text="60222105")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
 window.mainloop()
-
